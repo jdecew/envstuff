52ce9efb02a4a583f5d63fd6d26ed6efae354bc4
Jeff DeCew <jeff@vida.com>
2014-11-10 11:55:35 -0800

    Removing more crap

--- android/marmaladefix.sh
+++
@@ -1,4 +0,0 @@
-cd /Developer/Marmalade/6.2/modules/third_party/cocotron/
-cp cocotron.mkf cocotron.mkf.bak
-echo $1 | grep -E -q '^[01]$' || { echo 'Argument must 1 (Sim-preferred) or 0 (ARM-safe), "'$1'" provided' ; exit 1; }
-sed 's/{{ .* }}/{{ '$1' }}/' <cocotron.mkf.bak >cocotron.mkf
