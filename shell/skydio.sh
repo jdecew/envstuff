5aff2c8e7ba4b8d737d33afdade727c4d420bc6f
Jeff DeCew <jeff@skyd.io>
2016-01-06 14:02:17 -0800

    Update paths

--- shell/skydio.sh
+++ shell/skydio.sh
@@ -2,5 +2,9 @@
 export AIRCAM=~/aircam
 
 # add aircam tools
+# legacy
 export PATH=$PATH:$AIRCAM/build/bin
 export PATH=$PATH:$AIRCAM/build/third_party/bin
+# new
+export PATH=$PATH:$AIRCAM/build/host_aircam/bin
+export PATH=$PATH:$AIRCAM/build/host_third_party/bin
