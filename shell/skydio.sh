e10bc4268845280f95fd660d4ccb9cf0b2bb1318
Jeff DeCew <jeff@skyd.io>
2016-01-06 20:53:15 -0800

    fix path orders

--- shell/skydio.sh
+++ shell/skydio.sh
@@ -2,9 +2,10 @@
 export AIRCAM=~/aircam
 
 # add aircam tools
-# legacy
-export PATH=$PATH:$AIRCAM/build/bin
-export PATH=$PATH:$AIRCAM/build/third_party/bin
-# new
+# new paths
 export PATH=$PATH:$AIRCAM/build/host_aircam/bin
 export PATH=$PATH:$AIRCAM/build/host_third_party/bin
+export PATH=/usr/local/ccache:$PATH
+# legacy paths
+export PATH=$PATH:$AIRCAM/build/bin
+export PATH=$PATH:$AIRCAM/build/third_party/bin
