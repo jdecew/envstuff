be85b12fb23705d217adf61353ac9d49833357fd
Jeff DeCew <jeff@skydio.com>
2016-03-30 12:34:39 -0700

    Fix regex when rebasing

--- bash_utils.py
+++ bash_utils.py
@@ -116,7 +116,7 @@ def git_branch_status(rawargs):
         return 1
     lines = output.splitlines()
     rows = []
-    lm = re.compile("(?P<current>[*]?)\s*(?P<local>(\S|no branch|detached from [^ )]+)+)(?P<localpad>\s+?)(?P<sha>[0-9a-f]+)\s+(?P<fullmessage>(\[(?P<remote>[^]]+)\])? ?(?P<message>.*))$")
+    lm = re.compile("(?P<current>[*]?)\s*(?P<local>(\S|[(]no branch[^)]*[)]|[(]detached from [^)]+[)])+)(?P<localpad>\s+?)(?P<sha>[0-9a-f]+)\s+(?P<fullmessage>(\[(?P<remote>[^]]+)\])? ?(?P<message>.*))$")
     rm = re.compile("(?P<branch>\S+)(:|$)\s*(?P<gone>gone)?,?(ahead (?P<ahead>\d+))?,?\s*(behind (?P<behind>\d+))?") #origin/foo: ahead 5, behind 5
     for line in lines:
         line = line.strip()
