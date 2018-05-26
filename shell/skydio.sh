9d63cf899648b4d57b67a3d4b610a7fa9229cb67
Jeff DeCew <jeff@skydio.com>
2018-05-26 14:36:47 -0700

    add tests dir to the path

--- shell/skydio.sh
+++ shell/skydio.sh
@@ -1,6 +1,7 @@
 # add aircam tools
 export PATH=$PATH:./build/host_aircam/bin
 export PATH=$PATH:./build/host_third_party/bin
+export PATH=$PATH:./build/host_aircam/bin/tests
 
 # add python site packages to path (mac specific)
 export PYTHON_SITE=~/Library/Python/2.7/bin
