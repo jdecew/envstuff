9c31fcad14ab090a766f7f1a67329d6f21962a50
Jeff DeCew <jeff@skydio.com>
2019-12-17 14:30:07 -0800

    add yubikey utils

--- shell/skydio.sh
+++ shell/skydio.sh
@@ -18,3 +18,25 @@ export SKYREV_REMOTE_USER=jeff
 
 alias ca="cd ~/aircam"
 alias ca2="cd ~/aircam2"
+
+
+# Alias for Yubikey pin prompt
+alias yubact="ssh-add -e /usr/local/lib/opensc-pkcs11.so; ssh-add -s /usr/local/lib/opensc-pkcs11.so"
+
+# # Auto finds ssh-agent
+# . ~/yubikey_scripts/ssh-find-agent/ssh-find-agent.sh
+# ssh_find_agent -a
+# if [ -z "$SSH_AUTH_SOCK" ]
+# then
+#     eval $(ssh-agent) > /dev/null
+#     ssh-add -l >/dev/null || alias ssh='ssh-add -l >/dev/null || ssh-add && unalias ssh; ssh'
+# fi
+
+# Auto prompt yubikey on new terminal
+ioreg -p IOUSB | grep -i yubikey >/dev/null
+YUBIKEY_STATUS=$?
+ssh-add -L >/dev/null
+ACTIVE_STATUS=$?
+if [ $YUBIKEY_STATUS -eq 0 ] && [ $ACTIVE_STATUS -eq 1 ]; then
+    yubact
+fi
